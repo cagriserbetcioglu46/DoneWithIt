53923efe70eed2763cb3ccfd76ecdb4939a25f78
cagriserbetcioglu46 <cagri.serbetcioglu@st.uskudar.edu.tr>
2022-10-24 18:33:07 +0300

    son

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { StyleSheet, Image, Text, View, ImageBackground } from "react-native"
 //import Entry1 from "./interfaces/entryPage"
-import Login1 from "./interfaces/loginPage"
+//import Login1 from "./interfaces/loginPage"
+import SignUp1 from "./interfaces/signUpPage"
 
 
 export default function Frame1() {
   
-  return <Login1 />
+  return <SignUp1 />
   
 }
 
---
+++ interfaces/signUpPage.js
@@ -0,0 +1,145 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+
+
+export default function App() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require("../images/prosetlogo328-128.png")} />
+
+            <StatusBar style="auto" />
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Kullanıcı Adı"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Şifre"
+                    placeholderTextColor="#003f5c"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+            </View>  
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Host Numarası"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Port Numarası"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="SQL Şifresi"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+
+
+            <TouchableOpacity style={styles.kayitBtn}>
+                <Text style={styles.loginText}>Kayıt Ol</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FCE9D8",
+    },
+
+    image: {
+        marginBottom: 10,
+    },
+
+    inputView: {
+        backgroundColor: "#fff",
+        borderRadius: 15,
+        borderWidth: 1.,
+        borderColor: "#000",
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+
+        elevation: 24,
+    },
+
+    TextInput: {
+        height: 50,
+        flex: 1,
+        textAlign: "center",
+        padding: 10,
+    },
+
+
+    signUp_button: {
+        height: 30,
+        marginBottom: 10,
+        fontSize: 16,
+        color: "#a5240b",
+    },
+
+    kayitBtn: {
+        width: "40%",
+        borderRadius: 15,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 5,
+        backgroundColor: "#006400",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 24,
+    },
+
+    loginText: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+
+});
